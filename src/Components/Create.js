e5e3fdff26565af16ef32ba35140d88a2126d9b5
shahbaz shamshi <shahbazshamshi@Macs-MacBook-Pro-2.local>
2024-03-18 18:11:17 +0530

    form validate

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -6,29 +6,80 @@ function Create() {
   var [name, setName] = useState("");
   var [email, setEmail] = useState("");
   var [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessageForEmail, setErrorMessageForEmail] = useState("");
+  const [errorMessageForName, setErrorMessageForName] = useState("");
+
 
   function newNameData(event) {
-    console.log(event.target.value);
-    setName(event.target.value);
-  }
+
+let new_name=event.target.value;
+
+
+   
+
+
+
+    if (new_name.length < 4) {
+      setErrorMessageForName("Name should contains minimum 4 char!");
+    
+
+    }
+      else {
+        setErrorMessageForName("Perfect name!");
+        setName(new_name);
+      }
+    
+    }
 
   function newEmailData(event) {
-    console.log(event.target.value);
-    setEmail(event.target.value);
+    let new_email = event.target.value;
+    var lowerCase = /[a-z]/g;
+    var numbers = /[0-9]/g;
+    var special = /[@]/g;
+    var word = /[.com]/g;
+
+    if (!new_email.match(lowerCase)) {
+      setErrorMessageForEmail("email should contains lowercase letters!");
+    } else if (!new_email.match(numbers)) {
+      setErrorMessageForEmail("email should contain number!");
+    } else if (!new_email.match(special)) {
+      setErrorMessageForEmail("email should contains @ also!");
+    } else if (!new_email.match(word)) {
+      setErrorMessageForEmail("email should contains .com!");
+    } else if (new_email.length < 8) {
+      setErrorMessageForEmail("email length should be more than 8.");
+    } else {
+      setErrorMessageForEmail("Perfect email!");
+      setEmail(new_email);
+    }
   }
 
   function newPasswordData(event) {
-    console.log(event.target.value);
-    setPassword(event.target.value);
+    let new_pass = event.target.value;
+
+    var lowerCase = /[a-z]/g;
+    var upperCase = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+    if (!new_pass.match(lowerCase)) {
+      setErrorMessage("Password should contains lowercase letters!");
+    } else if (!new_pass.match(upperCase)) {
+      setErrorMessage("Password should contain uppercase letters!");
+    } else if (!new_pass.match(numbers)) {
+      setErrorMessage("Password should contains numbers also!");
+    } else if (new_pass.length < 8) {
+      setErrorMessage("Password length should be more than 8.");
+    } else {
+      setErrorMessage("Password is strong!");
+      setPassword(new_pass);
+    }
+
+    // if (!new_pass.match(valid)) {
+    //    setErrorMessage("type correct password!");
   }
-
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    console.log("submited");
-
 
     const header = { "Access-Control-Allow-Origin": "*" };
     axios.post("https://65e7f94f53d564627a8f9288.mockapi.io/crud", {
@@ -37,25 +88,17 @@ function Create() {
       password: password,
       header,
     });
-
-
-e.target.reset();
-
-
-
   };
 
   return (
     <form onSubmit={handleSubmit}>
-
       <div className="header">
-      <h2>Login</h2>
+        <h2>Login</h2>
 
-      <Link to="/read">
-              <button className="button"> Data</button>
-              </Link>
-
-       </div>
+        <Link to="/read">
+          <button className="button"> Data</button>
+        </Link>
+      </div>
 
       <div className="control-row">
         <div className="control no-margin">
@@ -67,6 +110,8 @@ e.target.reset();
             required=""
             onChange={newNameData}
           />
+
+<div style={{ color: "red" }}> {errorMessageForName} </div>
         </div>
 
         <div className="control no-margin">
@@ -78,29 +123,35 @@ e.target.reset();
             required=""
             onChange={newEmailData}
           />
+
+          <div style={{ color: "red" }}> {errorMessageForEmail} </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password"  required=""
-            onChange={newPasswordData}/>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required=""
+            onChange={newPasswordData}
+          />
+          <div style={{ color: "red" }}> {errorMessage} </div>
         </div>
       </div>
 
-      <p className="form-actions button-design">
-      <button type="reset" className="button button-flat"> Reset</button>
-       
-        <button type ="submit" className="button">Login</button>
+      <p className="form-actions ">
+        <button type="reset" className="button button-flat">
+          {" "}
+          Reset
+        </button>
 
-        
-            
-
+        <button type="submit" className="button">
+          Login
+        </button>
       </p>
     </form>
   );
 }
 
 export default Create;
-
-
-
---
+++ src/Components/login.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function Login() {
+  var [name, setName] = useState("");
+  var [email, setEmail] = useState("");
+  var [password, setPassword] = useState("");
+
+  function newNameData(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function newEmailData(event) {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  }
+
+  function newPasswordData(event) {
+    console.log(event.target.value);
+    setPassword(event.target.value);
+  }
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("submited");
+
+
+    const header = { "Access-Control-Allow-Origin": "*" };
+    axios.post("https://65e7f94f53d564627a8f9288.mockapi.io/crud", {
+      name: name,
+      email: email,
+      password: password,
+      header,
+    });
+
+
+e.target.reset();
+
+
+
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <div className="header">
+      <h2>Login</h2>
+
+      <Link to="/read">
+              <button className="button"> Data</button>
+              </Link>
+
+       </div>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Name</label>
+          <input
+            id="Name"
+            type="text"
+            name="name"
+            required=""
+            onChange={newNameData}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            required=""
+            onChange={newEmailData}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password"  required=""
+            onChange={newPasswordData}/>
+        </div>
+      </div>
+
+      <p className="form-actions button-design">
+      <button type="reset" className="button button-flat"> Reset</button>
+       
+        <button type ="submit" className="button">Login</button>
+
+        
+            
+
+      </p>
+    </form>
+  );
+}
+
+export default Login;
+
+
+
